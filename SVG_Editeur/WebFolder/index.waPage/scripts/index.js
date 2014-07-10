24513714cee2e732a304d21de3bb46ef01b22060
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-07-10 18:24:23 +0200

    Update #8

--- SVG_Editeur/WebFolder/index.waPage/scripts/index.js
+++ SVG_Editeur/WebFolder/index.waPage/scripts/index.js
@@ -2,20 +2,51 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var bSave = {};	// @button
 	var dgImages = {};	// @dataGrid
 // @endregion// @endlock
 
-	var Svg = $$("svg");
-	var $svg =  $("#svg");
+	//var Svg = $$("svg");
+	//var $svg =  $("#svgroot");
 
 // eventHandlers// @lock
 
-	dgImages.onRowClick = function dgImages_onRowClick (event)// @startlock
+	selImage();
+
+	bSave.click = function bSave_click (event)// @startlock
 	{// @endlock
-		// Add your code here
+		var $svg =  $("#svg").contents().find("#svgcontent");
+	//debugger;	
+//		if(svg != null)
+//			WAF.sources.mesImages.txtImage = $svg;
 	};// @lock
 
+	dgImages.onRowClick = function dgImages_onRowClick (event)// @startlock
+	{// @endlock
+		selImage();
+		
+	//debugger;
+		//var txt = WAF.sources.mesImages.txtImage.getValue;
+		
+//		if(WAF.sources.mesImages.txtImage != "")
+//			$svg.innerHTML(WAF.sources.mesImages.txtImage);
+			
+	};// @lock
+	
+	function selImage(){
+		
+		var $svg =  $("#svg").contents().find("#svgcontent");
+		var $aff =  $("#cAffImage");
+		var txt = "";
+		
+		if(WAF.sources.mesImages.txtImage != ""){
+			txt = WAF.sources.mesImages.txtImage;
+		}
+		$("#cAffImage").html(txt);
+	}
+
 // @region eventManager// @startlock
+	WAF.addListener("bSave", "click", bSave.click, "WAF");
 	WAF.addListener("dgImages", "onRowClick", dgImages.onRowClick, "WAF");
 // @endregion
 };// @endlock
